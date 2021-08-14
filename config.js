920600810eb5aa28238c756f976e013e18736f32
Khoi <khoi.hm@neo-lab.vn>
2021-08-14 14:37:17 +0700

    first commit

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
-    introTitle: 'Hee luu cậu!',
-    introDesc: `Tớ có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nha`,
+    introTitle: 'Chào em!',
+    introDesc: `Anh có điều này muốn hỏi, Em phải trả lời thật lòng nhé`,
     btnIntro: 'oke',
-    title: 'Cậu yêu tớ có phải không 😙',
-    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu là vợ tớ đó nha :> ',
-    btnYes: 'yêu ơi là iuuu <33',
-    btnNo: 'cậu mơ à :333',
+    title: 'Em còn thương Anh phải không?',
+    desc: 'Em không trả lời hoặc thoát ra thì Em vẫn còn đấy!!!!!!!',
+    btnYes: 'Thương ơi là thươnggg <33',
+    btnNo: 'Anh mơ à ~~',
     question:'Nói cho tới lý do vì sao cậu yêu tớ đii <3',
     btnReply: 'Gửi cho tớ <3',
-    reply: 'Tại vì cậu đẹp trai hihi <33333333',
-    mess: 'Tớ biết mà 🥰. Love you 300.000 😘😘',
-    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho tớ đi nào.',
+    reply: 'Tại vì Em rất thương Anh <3',
+    mess: 'Em làm người yêu anh nhé 🥰. I love u 3000 😘😘',
+    messDesc: 'Hãy để câu trả lời vào inbox anh nhé!!!',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/WhiskyIce' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
